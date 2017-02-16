fb33d51b7fa5ba697d2abc2920cc29c2c86777fc
Andrew Richardson <andrewrichardson@Andrews-MacBook-Pro.local>
2017-02-15 21:57:44 -0500

    exercise 1

--- Controllers/AutoController.cs
+++ Controllers/AutoController.cs
@@ -1,16 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using FisherInsuranceApi.Data;
+using FisherInsuranceApi.Models;
 
 [Route("api/auto/quotes")]
 public class AutoController : Controller
 {
+
+    private IMemoryStore db; 
+    public AutoController(IMemoryStore repo) 
+    {  
+        db = repo; 
+    }
     // POST api/auto/quotes
     [HttpPost]
 
-    public IActionResult Post([FromBody]string value) 
+    public IActionResult Post([FromBody]Quote quote) 
     {
-
-    return Created("", value);
-
+        return Ok(db.CreateQuote(quote));
     }
 
     // GET api/auto/quotes/5
@@ -19,17 +25,23 @@ public class AutoController : Controller
 
     public IActionResult Get(int id) 
     {
-           return Ok("The id is: " + id);
+           return Ok(db.RetrieveQuote(id));
+    }
+
+    [HttpGet] 
+    public IActionResult GetQuotes() 
+    {  
+        return Ok(db.RetrieveAllQuotes); 
     }
 
     // PUT api/auto/quotes/putid
 
     [HttpPut("{id}")]
 
-    public IActionResult Put(int id, [FromBody]string value) 
+    public IActionResult Put(int id, [FromBody]Quote quote) 
     {
 
-        return NoContent();
+        return Ok(db.UpdateQuote(quote)); 
     }
 
     // DELETE api/auto/quotes/delid
@@ -38,7 +50,8 @@ public class AutoController : Controller
 
     public IActionResult Delete(int id) 
     {
-        return Delete(id);
+        db.DeleteQuote(id);
+        return Ok();
     }
 
 
