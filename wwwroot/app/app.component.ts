a9711a6c6f7807d509ba414d5d094378e6647662
Andrew Richardson <andrewrichardson@Andrews-MacBook-Pro.local>
2017-03-05 18:51:22 -0500

    modification

--- wwwroot/app/app.component.ts
+++ wwwroot/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 @Component({
     selector: "fisher-insurance",
-    templateUrl: './app/app.component.html'
+    template: '<home-page>'
 })
 export class AppComponent { }
