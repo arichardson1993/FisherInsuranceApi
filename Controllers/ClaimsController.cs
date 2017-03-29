bdb9996e308db1b96c73f20536d510e9e80a9921
Andrew Richardson <andrewrichardson@Andrews-MacBook-Pro.local>
2017-03-29 19:01:42 -0400

    final lab 7

--- Controllers/ClaimsController.cs
+++ Controllers/ClaimsController.cs
@@ -1,76 +1,77 @@
-using Microsoft.AspNetCore.Mvc;
-using FisherInsuranceApi.Models;
 using FisherInsuranceApi.Data;
+using FisherInsuranceApi.Models;
+using Microsoft.AspNetCore.Mvc;
 
-[Route("api/claims/claim")]
-public class ClaimsController : Controller
+namespace FisherInsuranceApi.Controllers
 {
-    private readonly FisherContext db;
-    public ClaimsController(FisherContext context)
+    [RouteAttribute("api/claims")]
+    public class ClaimsController : Controller
     {
-      
-      db = context;
 
-    } 
-    // POST api/claims/claim
-    [HttpPost]
-    public IActionResult Post([FromBody]Claim claim) 
-    {
-        var newClaim = db.Claims.Add(claim);
-        db.SaveChanges();
+        private readonly FisherContext db;
 
-        return CreatedAtRoute("GetClaim", new {id = claim.Id}, claim);
-    }
-
-    // GET api/claims/claim/5
-
-    [HttpGet("{id}", Name = "GetClaim")]
-
-    public IActionResult Get(int id) 
-    {
-           return Ok(db.Claims.Find(id)); 
-    }
-
-    [HttpGet]
-    public IActionResult Claims()
-    {
-        return Ok(db.Claims);
-    } 
-
-    // PUT api/claims/claim/id
-
-    [HttpPut("{id}")]
-
-    public IActionResult Put(int id, [FromBody]Claim claim) 
-    {
-        var newClaim = db.Claims.Find(id);
-        if (newClaim == null)
+        public ClaimsController(FisherContext context)
         {
-            return NotFound();
+            db = context;
         }
 
-        newClaim = claim;
-        db.SaveChanges();
-        return Ok(newClaim);
+        [HttpGet]
+        public IActionResult GetClaims()
+        {
+            return Ok(db.Claims);
+
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Claim claim)
+        {
+            var newClaim = db.Claims.Add(claim);
+            db.SaveChanges();
+
+            return CreatedAtRoute("GetClaim", new { id = claim.Id }, claim);
+        }
+
+
+        [HttpGet("{id}", Name = "GetClaim")]
+        public IActionResult Get(int id)
+        {
+            return Ok(db.Claims.Find(id));
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Claim newClaim)
+        {
+            var claim = db.Claims.Find(id);
+            if (claim == null)
+            {
+                return NotFound();
+            }
+
+            claim = newClaim;
+            claim.Id = id;
+
+            db.Update(claim);
+            db.SaveChanges();
+            
+            return Ok(claim);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var claimToDelete = db.Claims.Find(id);
+            if (claimToDelete == null)
+            {
+                return NotFound();
+            }
+
+            db.Claims.Remove(claimToDelete);
+            db.SaveChangesAsync();
+
+            return NoContent();
+
+        }
     }
-
-    // DELETE api/claims/id
-
-    [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
-   {
-       var claimToDelete = db.Claims.Find(id);
-       if (claimToDelete == null)
-       {
-
-           return NotFound();
-
-       }
-
-       db.Claims.Remove(claimToDelete);
-       db.SaveChangesAsync();
-        
-        return NoContent();
-   }
-
 }
--- Data/FisherContext.cs
+++ Data/FisherContext.cs
@@ -1,20 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using FisherInsuranceApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 namespace FisherInsuranceApi.Data
-{    
-    public class FisherContext : DbContext     
-    {    
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)        
-        {            
-            string connection = "User ID=fisher-user;Password=password ;Host=localhost;Port=5432;Database=fisher-insurance;Pooling=true;";            
-            optionsBuilder.UseNpgsql(connection);         
-        }
-
+{
+    public class FisherContext : IdentityDbContext<ApplicationUser>
+    {
         public DbSet<Claim> Claims { get; set; }
         public DbSet<Quote> Quotes { get; set; }
 
-    
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = "User ID=fisher;Password=fisher;Host=localhost;Port=5432;Database=fisher-insurance;Pooling=false;";
+
+            optionsBuilder.UseNpgsql(connection);
+        }
     }
-    
 }
